c0c83fee8a8ba99bbffca29ea3ea0d21a9898cd1
uai <forgivemyblue13@gmail.com>
2019-07-03 02:50:13 +0900

    URLとUser-Agent追加

--- extract_title_1.py
+++ extract_title_1.py
@@ -8,17 +8,21 @@ import csv
 #検索キーワード
 list_keywd = ['python']
 #検索数
-search_num = 100
+search_num = 10
 #GoogleのURL
 search_URL = 'https://www.google.co.jp/search?num='+ str(search_num)
+#User-Agent
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36 "}
+
 
 if list_keywd:
     #Googleに接続
-    request_Google = requests.get(search_URL, params={'q':' '.join(list_keywd) })
+    request_Google = requests.get(search_URL, params={'q':' '.join(list_keywd) },headers = headers)
     request_Google.raise_for_status()
 
     #BeautifulSoupで掲載サイトのURLを取得
     bs4_Google = BeautifulSoup(request_Google.text,"html.parser")
+
 
     #scriptタグを削除
     for script in bs4_Google.find_all('script',src = False):
@@ -26,11 +30,15 @@ if list_keywd:
     #styleタグを削除
     for style in bs4_Google.find_all('style',src = False):
         style.decompose()
-    #print(bs4_Google.prettify())
+
+    print(bs4_Google.prettify())
 
     #検索結果のタイトルを取得
-    title = bs4_Google.find_all('div',{'class':'BNeawe vvjwJb AP7Wnd'})
+    title = bs4_Google.find_all('h3',{'class':'LC20lb'})
+    url = bs4_Google.find_all('cite',{'class':'iUh30'})
 
     #出力
-    for index, tl in enumerate(title) :
-        print(str(index)+ ": " + tl.text)
+    for (tl,ul) in zip(title, url) :
+        print(" ・ " + tl.text)
+        print("     <"+ ul.text+">")
+        print("\n")
